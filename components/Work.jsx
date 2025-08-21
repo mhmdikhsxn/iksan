012d13338bb0ccb0617c8a221626399779b15a6d
mhmdikhsxn <sxncreative@gmail.com>
2025-08-21 17:10:30 +0700

    upt

--- components/Work.jsx
+++ components/Work.jsx
@@ -14,7 +14,7 @@ const Work = ({isDarkMode}) => {
         My latest work
       </motion.h2>
 
-      <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.7, duration: 0.5 }} className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+      <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.7, duration: 0.5 }} className="text-center max-w-2xl mx-auto mt-5 mb-12 text-sm font-Ovo">
         Explore my portfolio to see how i use the latest technologies to build impactful digital solutions that help individual, startups, adn business growth.
       </motion.p>
 
