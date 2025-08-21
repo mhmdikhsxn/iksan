3de102d716ea8da01817e5f997a9252f6d0a4a79
mhmdikhsxn <sxncreative@gmail.com>
2025-08-21 20:17:09 +0700

    updesclang

--- assets/assets.js
+++ assets/assets.js
@@ -107,7 +107,7 @@ export const serviceData = [
 ]
 
 export const infoList = [
-  { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: "Languages", description: "HTML, CSS, JavaScript" },
+  { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: "Languages", description: "HTML, CSS, JavaScript,TailwindCSS, MySQL, NextJS, NodeJS, Pythom, ReactJS, VueJS, MongoDB" },
   { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: "Education", description: "Mechatronics Engineering at SMKN 2 Sukoharjo"},
   { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: "Projects", description: "Built more than 5 projects" },
 ];
