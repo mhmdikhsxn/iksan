87ce62773619958c41b981d0b3dc7a86b988c090
mhmdikhsxn <sxncreative@gmail.com>
2025-08-21 16:55:29 +0700

    ya

--- assets/assets.js
+++ assets/assets.js
@@ -108,7 +108,7 @@ export const serviceData = [
 
 export const infoList = [
   { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: "Languages", description: "HTML, CSS, JavaScript" },
-  { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: "Education", description: "Mechatronics Engineering in SMK Negeri 2 Sukoharjo" },
+  { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: "Education", description: "Mechatronics Engineering in SMKN 2 Sukoharjo" },
   { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: "Projects", description: "Built more than 5 projects" },
 ];
 
